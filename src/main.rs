4879af391541940ee969d7a832983bd0f6e184b4
Arijit Basu <sayanarijit@gmail.com>
2021-04-26 12:35:15 +0530

    zellij experiment

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,40 @@
 use std::env;
 use std::path::PathBuf;
 use xplr::app;
-use xplr::runner;
 
-fn main() {
-    let mut pwd = PathBuf::from(env::args().nth(1).unwrap_or_else(|| ".".into()))
-        .canonicalize()
-        .unwrap_or_default();
-    let mut focused_path = None;
+use zellij_tile::prelude::*;
 
-    if pwd.is_file() {
-        focused_path = Some(
-            pwd.file_name()
-                .unwrap_or_default()
-                .to_string_lossy()
-                .to_string(),
-        );
-        pwd = pwd.parent().map(|p| p.into()).unwrap_or_default();
-    }
+#[derive(Default)]
+struct State;
 
-    let app = app::App::create(pwd).unwrap_or_else(|e| {
-        eprintln!("error: {}", e);
-        std::process::exit(1);
-    });
+register_plugin!(State);
 
-    match runner::run(app, focused_path) {
-        Ok(Some(out)) => print!("{}", out),
-        Ok(None) => {}
-        Err(err) => {
-            if !err.to_string().is_empty() {
-                eprintln!("error: {}", err);
-            };
+impl ZellijPlugin for State {
+    fn load(&mut self) {}
 
-            std::process::exit(1);
+    fn update(&mut self, event: Event) {}
+
+    fn render(&mut self, rows: usize, cols: usize) {
+        let mut pwd = PathBuf::from(env::args().nth(1).unwrap_or_else(|| ".".into()))
+            .canonicalize()
+            .unwrap_or_default();
+        let mut focused_path = None;
+
+        if pwd.is_file() {
+            focused_path = Some(
+                pwd.file_name()
+                    .unwrap_or_default()
+                    .to_string_lossy()
+                    .to_string(),
+            );
+            pwd = pwd.parent().map(|p| p.into()).unwrap_or_default();
         }
+
+        let app = app::App::create(pwd).unwrap_or_else(|e| {
+            eprintln!("error: {}", e);
+            std::process::exit(1);
+        });
+
+        println!("foo");
     }
 }
--- src/runner.rs
+++ src/runner.rs
@@ -2,14 +2,11 @@
 
 use crate::app;
 use crate::auto_refresher;
-use crate::event_reader;
 use crate::explorer;
 use crate::pipe_reader;
 use crate::pwd_watcher;
 use crate::ui;
 use anyhow::Result;
-use crossterm::execute;
-use crossterm::terminal as term;
 use handlebars::{handlebars_helper, Handlebars};
 use humansize::{file_size_opts as options, FileSize};
 use std::fs;
@@ -17,8 +14,6 @@ use std::io;
 use std::io::prelude::*;
 use std::process::{Command, ExitStatus, Stdio};
 use std::sync::mpsc;
-use termion::get_tty;
-use tui::backend::CrosstermBackend;
 use tui::Terminal;
 
 handlebars_helper!(to_humansize: |size: i64| size.file_size(options::CONVENTIONAL).unwrap_or_default());
@@ -33,7 +28,7 @@ fn call(app: &app::App, cmd: app::Command, silent: bool) -> io::Result<ExitStatu
     let (stdin, stdout, stderr) = if silent {
         (Stdio::null(), Stdio::null(), Stdio::null())
     } else {
-        (get_tty()?.into(), get_tty()?.into(), get_tty()?.into())
+        (Stdio::inherit(), Stdio::inherit(), Stdio::inherit())
     };
 
     Command::new(cmd.command().clone())
@@ -64,183 +59,4 @@ fn call(app: &app::App, cmd: app::Command, silent: bool) -> io::Result<ExitStatu
         .stderr(stderr)
         .args(cmd.args())
         .status()
-}
-
-pub fn run(mut app: app::App, focused_path: Option<String>) -> Result<Option<String>> {
-    let (tx_msg_in, rx_msg_in) = mpsc::channel();
-    let (tx_event_reader, rx_event_reader) = mpsc::channel();
-    let (tx_pwd_watcher, rx_pwd_watcher) = mpsc::channel();
-
-    explorer::explore(
-        app.explorer_config().clone(),
-        app.pwd().clone(),
-        focused_path,
-        tx_msg_in.clone(),
-    );
-
-    let mut hb = Handlebars::new();
-    hb.register_helper("humansize", Box::new(to_humansize));
-    hb.register_template_string(
-        app::TEMPLATE_TABLE_ROW,
-        &app.config()
-            .general()
-            .table()
-            .row()
-            .cols()
-            .clone()
-            .unwrap_or_default()
-            .iter()
-            .map(|c| c.format().clone().unwrap_or_default())
-            .collect::<Vec<String>>()
-            .join("\t"),
-    )?;
-
-    let mut result = Ok(None);
-
-    term::enable_raw_mode()?;
-    let mut stdout = get_tty()?;
-    // let mut stdout = stdout.lock();
-    execute!(stdout, term::EnterAlternateScreen)?;
-    // let stdout = MouseTerminal::from(stdout);
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.hide_cursor()?;
-
-    // Threads
-    auto_refresher::start_auto_refreshing(tx_msg_in.clone());
-    pipe_reader::keep_reading(app.pipe().msg_in().clone(), tx_msg_in.clone());
-    event_reader::keep_reading(tx_msg_in.clone(), rx_event_reader);
-    pwd_watcher::keep_watching(app.pwd(), tx_msg_in.clone(), rx_pwd_watcher)?;
-
-    'outer: for task in rx_msg_in {
-        let last_app = app.clone();
-
-        let (new_app, new_result) = match app.handle_task(task, &last_app) {
-            Ok(a) => (a, Ok(None)),
-            Err(err) => (last_app.clone(), Err(err)),
-        };
-
-        app = new_app;
-        result = new_result;
-
-        if result.is_err() {
-            break;
-        }
-
-        while let Some(msg) = app.pop_msg_out() {
-            match msg {
-                app::MsgOut::Enque(task) => {
-                    tx_msg_in.send(task)?;
-                }
-
-                app::MsgOut::Quit => {
-                    result = Ok(None);
-                    break 'outer;
-                }
-
-                app::MsgOut::PrintResultAndQuit => {
-                    result = Ok(Some(app.result_str()));
-                    break 'outer;
-                }
-
-                app::MsgOut::PrintAppStateAndQuit => {
-                    let out = serde_yaml::to_string(&app)?;
-                    result = Ok(Some(out));
-                    break 'outer;
-                }
-
-                app::MsgOut::Debug(path) => {
-                    fs::write(&path, serde_yaml::to_string(&app)?)?;
-                }
-
-                app::MsgOut::ClearScreen => {
-                    terminal.clear()?;
-                }
-
-                app::MsgOut::Explore => {
-                    explorer::explore(
-                        app.explorer_config().clone(),
-                        app.pwd().clone(),
-                        app.focused_node().map(|n| n.relative_path().clone()),
-                        tx_msg_in.clone(),
-                    );
-                }
-
-                app::MsgOut::Refresh => {
-                    if app.pwd() != last_app.pwd() {
-                        tx_pwd_watcher.send(app.pwd().clone())?;
-                        explorer::explore(
-                            app.explorer_config().clone(),
-                            app.pwd().clone(),
-                            app.focused_node().map(|n| n.relative_path().clone()),
-                            tx_msg_in.clone(),
-                        );
-                    };
-
-                    // UI
-                    terminal.draw(|f| ui::draw(f, &app, &hb))?;
-                }
-
-                app::MsgOut::CallSilently(cmd) => {
-                    tx_event_reader.send(true)?;
-
-                    let status = call(&app, cmd, false)
-                        .map(|s| {
-                            if s.success() {
-                                Ok(())
-                            } else {
-                                Err(format!("process exited with code {}", &s))
-                            }
-                        })
-                        .unwrap_or_else(|e| Err(e.to_string()));
-
-                    if let Err(e) = status {
-                        let msg = app::MsgIn::External(app::ExternalMsg::LogError(e));
-                        tx_msg_in.send(app::Task::new(msg, None))?;
-                    };
-
-                    tx_event_reader.send(false)?;
-                }
-
-                app::MsgOut::Call(cmd) => {
-                    tx_event_reader.send(true)?;
-
-                    terminal.clear()?;
-                    terminal.set_cursor(0, 0)?;
-                    term::disable_raw_mode()?;
-                    terminal.show_cursor()?;
-
-                    let status = call(&app, cmd, false)
-                        .map(|s| {
-                            if s.success() {
-                                Ok(())
-                            } else {
-                                Err(format!("process exited with code {}", &s))
-                            }
-                        })
-                        .unwrap_or_else(|e| Err(e.to_string()));
-
-                    if let Err(e) = status {
-                        let msg = app::MsgIn::External(app::ExternalMsg::LogError(e));
-                        tx_msg_in.send(app::Task::new(msg, None))?;
-                    };
-
-                    terminal.clear()?;
-                    term::enable_raw_mode()?;
-                    terminal.hide_cursor()?;
-                    tx_event_reader.send(false)?;
-                }
-            };
-        }
-    }
-
-    terminal.clear()?;
-    terminal.set_cursor(0, 0)?;
-    execute!(terminal.backend_mut(), term::LeaveAlternateScreen)?;
-    term::disable_raw_mode()?;
-    terminal.show_cursor()?;
-
-    fs::remove_dir_all(app.session_path())?;
-
-    result
 }
